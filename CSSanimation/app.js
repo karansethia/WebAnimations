09de03905e3f0b5e8b58407d1ad0b03d1efab392
Karan <karansethia24@gmail.com>
2022-09-28 22:37:43 +0530

    Revising js and css and creating animations

---
+++ CSSanimation/app.js
@@ -0,0 +1,3 @@
+document.getElementsByClassName('animate').addEventListener('mouseover',function hoverr(){
+    document.getElementsByClassName('animate').innerHTML = '<p class="text">this should work</p>'
+})
