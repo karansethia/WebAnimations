0efbf52006c1977ed78d2dfbb9a903cdd686e1db
Karan <karansethia24@gmail.com>
2022-10-01 11:31:02 +0530

    playing with css keyframes

--- CSSanimation/app.js
+++
@@ -1,3 +0,0 @@
-document.getElementsByClassName('animate').addEventListener('mouseover',function hoverr(){
-    document.getElementsByClassName('animate').innerHTML = '<p class="text">this should work</p>'
-})
